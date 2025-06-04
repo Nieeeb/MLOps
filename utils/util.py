8bcba09f1630cc96141f1145ebe074c868e7d958
VictorSteinrud <138033799+VictorSteinrud@users.noreply.github.com>
2025-06-04 11:15:42 +0200

    staged merge

--- utils/util.py
+++ utils/util.py
@@ -1,24 +1,25 @@
 from nets.net import Net
 import torch
-
-
-
-def load_model(train: bool=True,
-               checkpoint: 
-               ):
-    
-    model = Net()
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    model.to(device)
-
-    if train:
-        model.train()
-    else:
-        model.eval()
-    return model
 import random
 import numpy as np
-import torch
+
+
+
+
+# def load_model(train: bool=True,
+#                checkpoint: 
+#                ):
+    
+#     model = Net()
+#     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+#     model.to(device)
+
+#     if train:
+#         model.train()
+#     else:
+#         model.eval()
+#     return model
+
 
 
 def setup_seed(seed: int = 0) -> None:
