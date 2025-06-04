a2ab5a7945a60387ff51de9fe2b7a992cda37b08
Nieeeb <william_nieboer@hotmail.com>
2025-06-04 11:12:30 +0200

    added data downloading and dataloader creation

---
+++ utils/util.py
@@ -0,0 +1,11 @@
+import random
+import numpy as np
+import torch
+
+
+def setup_seed(seed: int = 0) -> None:
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True
